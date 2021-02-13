e74e4a234a92dbbf6b166d0d4e3bac5b90029085
Dávid Hezký <david.hezky@cdis.cz>
2021-02-13 17:47:37 +0100

    mod: update pckg dh-scripts-run

--- src/js/consts/dirs.js
+++ src/js/consts/dirs.js
@@ -5,5 +5,5 @@ const PROJECT_NAME = "dh-scripts-web";
 const STATIC_DIR = `${DIRS.CWD_NODE_MODULES}/${PROJECT_NAME}/static`;
 const STATIC_INDEX_HTML = `${STATIC_DIR}/index.html`;
 
-export * from "dh-scripts-run/consts/dirs";
+export * from "dh-scripts-run/js/consts/dirs";
 export { STATIC_DIR, STATIC_INDEX_HTML };
