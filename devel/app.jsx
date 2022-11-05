65dd2168bb06477059abc92c47a9bcb7626be74c
Dávid Hezký <david.hezky@cdis.cz>
2022-11-05 21:57:27 +0100

    fix: refactoring file names

--- devel/app.jsx
+++ devel/app.jsx
@@ -3,8 +3,6 @@ import "regenerator-runtime/runtime";
 import React from "react";
 import ReactDOM from "react-dom";
 
-console.log("jsem zde");
-
 import AppContainer from "./AppContainer";
 
 ReactDOM.render(<AppContainer />, document.getElementById("root"));
